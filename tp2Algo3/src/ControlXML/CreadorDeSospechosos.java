8065673e92fbc906535549cb13c8cab61d22405d
chrishaig14 <chrishaig14@5fe92814-5a1e-d52a-0bc9-2235f96b8868>
2014-06-13 02:23:13 +0000

    Hice el creadorDeSospechosos, también hice el XML de los sospechosos.

--- tp2Algo3/src/ControlXML/CreadorDeSospechosos.java
+++ tp2Algo3/src/ControlXML/CreadorDeSospechosos.java
@@ -1,22 +1,52 @@
 package ControlXML;
+import org.w3c.dom.*;
+import org.xml.sax.SAXException;
 
+import javax.xml.parsers.*;
+import java.io.IOException;
 import descripciones.Descripcion;
 import personajes.Sospechoso;
 
 import java.util.ArrayList;
 
-/**
- * Created by chris on 11/06/2014.
- */
 public class CreadorDeSospechosos {
-    //Lo creamos con el XML de los sospechosos
+    private String f;
+    private ArrayList<Sospechoso> sospechosos;
+
+    public CreadorDeSospechosos(String f) {
+        DocumentBuilderFactory documentBuilderFactory=DocumentBuilderFactory.newInstance();
+        try {
+            DocumentBuilder documentBuilder=documentBuilderFactory.newDocumentBuilder();
+            Document document=documentBuilder.parse(f);
+            Element element=document.getDocumentElement();
+            NodeList sospechososList=element.getChildNodes();
+            for (int i = 0; i < sospechososList.getLength(); i++) {
+                if(sospechososList.item(0).getNodeType()==Node.ELEMENT_NODE){
+                    Element e= (Element) sospechososList.item(i);
+                    Descripcion d=crearDescripcion(e);
+                    Sospechoso s=new Sospechoso(e.getAttribute("nombre"),d);
+                    sospechosos.add(s);
+                }
+            }
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        } catch (SAXException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
     public ArrayList<Sospechoso> obtenerListaDeSospechosos(){
-        return null;
+        return sospechosos;
     }
-    private Sospechoso crearSospechoso(Descripcion descripcion){
-        return new Sospechoso(descripcion);
-    }
-    private Descripcion crearDescripcion(){
-        return null;
+    private Descripcion crearDescripcion(Element e){
+        String sexo=e.getAttribute("sexo");
+        String hobby=e.getAttribute("hobby");
+        String pelo=e.getAttribute("pelo");
+        String senia=e.getAttribute("senia");
+        String vehiculo=e.getAttribute("vehiculo");
+        return new Descripcion(sexo,hobby,pelo,senia,vehiculo);
+
     }
 }
