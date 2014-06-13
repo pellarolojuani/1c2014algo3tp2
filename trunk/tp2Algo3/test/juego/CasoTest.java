a6a61367cd8458e8071fcffe8ee42846f7de7bbd
anze.frank@gmail.com <anze.frank@gmail.com@5fe92814-5a1e-d52a-0bc9-2235f96b8868>
2014-06-13 21:46:09 +0000

    Agrego tests unitarios

---
+++ trunk/tp2Algo3/test/juego/CasoTest.java
@@ -0,0 +1,29 @@
+package juego;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CasoTest {
+
+	@Test
+	public void testCaso() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testObtenerCiudadRobo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testObtenerObjetoRobado() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testObtenerSexoLadron() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ trunk/tp2Algo3/test/juego/JuegoTest.java
@@ -0,0 +1,29 @@
+package juego;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class JuegoTest {
+
+	@Test
+	public void testJuego() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAsignarPolicia() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCrearCaso() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testObtenerCaso() {
+		fail("Not yet implemented");
+	}
+
+}
