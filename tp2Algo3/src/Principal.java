e174da337ccf516f3c23e27806f5a61641018fd9
pellarolojuani@gmail.com <pellarolojuani@gmail.com@5fe92814-5a1e-d52a-0bc9-2235f96b8868>
2014-06-22 19:28:37 +0000

    modificaciones en la vista. Agregada una imagen a la vista principal.

--- tp2Algo3/src/Principal.java
+++ tp2Algo3/src/Principal.java
@@ -1,15 +1,20 @@
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+
 import Vista.*;
 import controlador.ControlMenu.*;
 
-public class Principal {
-	
-
+public class Principal{
 	
 	public static void main(String args[]){
 		
-		Controlador control = new Controlador();
+		//Controlador control = new Controlador();
 		
-		new Vista(control);
+		//new Vista(control);
+		VistaPrincipal vista = new VistaPrincipal();
+		vista.setVisible(true);
 	}
+	
 
 }
