5649f8d1012cb90e766be889140a067ee6639ac1
nicotruk@gmail.com <nicotruk@gmail.com@5fe92814-5a1e-d52a-0bc9-2235f96b8868>
2014-06-23 01:47:01 +0000

    Ya funciona el menu Acerca De->Creditos...

--- trunk/tp2Algo3/src/Vista/VistaPrincipal.java
+++ trunk/tp2Algo3/src/Vista/VistaPrincipal.java
@@ -1,7 +1,6 @@
 package Vista;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
+import javax.swing.*;
 
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
@@ -18,19 +17,26 @@ import java.util.*;
 import java.awt.event.*;
 
 public class VistaPrincipal extends JFrame implements Observer{
-	
-	private Frame frame;
+
 	private MenuBar menuBarra;
 	private Menu menu1, menu2, menu3;
-	MenuItem nuevo, guardar, salir;
+	MenuItem nuevo, guardar, salir, creditos;
 	
 	private static final long serialVersionUID = 1L;
 	
-	//Clase auxiliar para escuchar el evento de cerrado de la ventana
+	//Clase auxiliar para escuchar el evento de cerrado de la ventana principal
 	public static class CloseListener extends WindowAdapter
 	{	public void windowClosing(WindowEvent e)
 		{	e.getWindow().setVisible(false);
 			System.exit(0);
+		}
+	}
+	
+	//Clase auxiliar para escuchar el evento de cerrado de un Frame secundario
+	public static class CloseFrameListener extends WindowAdapter {
+		public void windowClosing(WindowEvent e){
+			e.getWindow().setVisible(false);
+			e.getWindow().dispose();
 		}
 	}
 
@@ -39,8 +45,7 @@ public class VistaPrincipal extends JFrame implements Observer{
 		Controlador control = new Controlador();
 		
 		this.setName("ALGOTHIEF GRUPO X");
-		this.
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(50, 50, 700, 500);
 		VistaPrincipalImagen p = new VistaPrincipalImagen();
 		p.setBorder(new EmptyBorder(5, 5, 5, 5));
@@ -64,6 +69,10 @@ public class VistaPrincipal extends JFrame implements Observer{
 		menu2 = new Menu("Opciones");
 		
 		menu3 = new Menu("Acerca de");
+		creditos = new MenuItem("Creditos");
+		creditos.addActionListener(getListenerCreditos());
+		
+		menu3.add(creditos);
 		
 		menuBarra.add(menu1);
 		menuBarra.add(menu2);
@@ -74,6 +83,38 @@ public class VistaPrincipal extends JFrame implements Observer{
 		
 	}
 
+	private class EscuchaBotonCreditos implements ActionListener{
+		public void actionPerformed(ActionEvent e){
+			Frame frameCreditos = new Frame("Creditos");
+			frameCreditos.setSize(400,300);  //seteamos las dimensiones del marco
+			frameCreditos.setLocationRelativeTo(rootPane);
+			frameCreditos.addWindowListener(new CloseFrameListener());
+			JLabel label = new JLabel();
+			label.setSize(frameCreditos.getSize());
+			String texto = "<html><body>Este juego fue creado por alumnos de la Facultad de Ingenieria" +
+					" de la Universidad de Buenos Aires, correspondiendo al Trabajo" +
+					" Practico Grupal de la materia 'Algoritmos y Programacion 3, " +
+					"Catedra Carlos Fontela'.<br><br>" +
+					"Sus creadores son los siguientes:<br><br>" +
+					"-Christian Pedersen<br>" +
+					"-Frank Douglas Anze<br>" +
+					"-Juan Ignacio Pellarolo<br>" +
+					"-Nicolas Truksinas<br>" +
+					"-Sebastian Savulsky<br>" +
+					"</body></html>";
+			label.setText(texto);
+			frameCreditos.add("North", label);
+			frameCreditos.setVisible(true);  //mostramos el marco
+			
+		}
+	}
+	
+	public ActionListener getListenerCreditos() {
+		return new EscuchaBotonCreditos();
+	}
+
+	
+	
 	public void update(Observable arg0, Object arg1) {
 		// TODO Auto-generated method stub
 		
