ce5ffa76e30166248d542e29f03556290f087b51
sasavulsky@yahoo.com.ar <sasavulsky@yahoo.com.ar@5fe92814-5a1e-d52a-0bc9-2235f96b8868>
2014-06-30 20:49:29 +0000

    Se modificó la clase main por Main según la codificación estándar de java. Se modificó la excepción de tiempo y el Menu base. Falta codificar mucho del menú y la vista. Falta codificar muchas excepciones y pruebas.

--- tp2Algo3/src/modelo/elementosDelJuego/SeAcaboElTiempoDelCasoExcepcion.java
+++ tp2Algo3/src/modelo/elementosDelJuego/SeAcaboElTiempoDelCasoExcepcion.java
@@ -1,5 +1,12 @@
 package modelo.elementosDelJuego;
 
 public class SeAcaboElTiempoDelCasoExcepcion extends RuntimeException{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 
+	public String AvisoAlJugador(){
+		return ("GAME OVER, partida nueva? S o N?");
+	}
 }
