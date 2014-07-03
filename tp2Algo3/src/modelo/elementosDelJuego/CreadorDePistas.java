677826ac1181deb3b06d85ff33db04f34e0e1de4
anze.frank@gmail.com <anze.frank@gmail.com@5fe92814-5a1e-d52a-0bc9-2235f96b8868>
2014-07-03 14:40:24 +0000

    Mejoro un poco el código y cambio como y cuando mostrar las pistas del sospechoso

--- tp2Algo3/src/modelo/elementosDelJuego/CreadorDePistas.java
+++ tp2Algo3/src/modelo/elementosDelJuego/CreadorDePistas.java
@@ -30,35 +30,37 @@ public class CreadorDePistas {
     private String textoHobby;
     private String textoVehiculo;
 
-    private String textoFauna;
-    private String textoIndustria;
-    private String textoIdioma;
-    private String textoPersonaje;
-    private String textoLugarinteres;
-    private String textoBanderaAeropuerto;
-    private String textoBanderaPuerto;
-    private String textoMoneda;
-    private ArrayList<Pista> pistasLadron;
-    private RecorridoLadron recorridoLadron;
-    private Boolean heridaCuchillo;
-    private Boolean heridaPistola;
-    private int aux_heridaCuchillo; //es un contador que utilizo para saber a partir de que ciudad
-    //puede haber herida de cuchillo
-    private int aux_heridaPistola;
-    private Dificultad dificultad;
 
-
-    public CreadorDePistas(RecorridoLadron unRecorrido, Sospechoso ladron) {
-
-        this.recorridoLadron = unRecorrido;
-        this.ladron = ladron;
-        this.heridaCuchillo = false;
-        this.heridaPistola = false;
-
-        DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory
-                .newInstance();
-        try {
-            File xmlFile = new File("pistas.xml");
+	private String textoFauna;
+	private String textoIndustria;
+	private String textoIdioma;
+	private String textoPersonaje;
+	private String textoLugarinteres;
+	private String textoBanderaAeropuerto;
+	private String textoBanderaPuerto;
+	private String textoMoneda;
+	private ArrayList<Pista> pistasLadron; //Aca guardo las pistas de la descripcion del sospechoso
+	private RecorridoLadron recorridoLadron;
+	private Boolean heridaCuchillo;
+	private Boolean heridaPistola;
+	private int aux_heridaCuchillo; //es un contador que utilizo para saber a partir de que ciudad
+									//puede haber herida de cuchillo
+	private int aux_heridaPistola;
+	private Valor dificultad;
+	
+    
+    public CreadorDePistas(RecorridoLadron unRecorrido, Sospechoso ladron, Valor nivel_dificultad){
+    	
+    	this.recorridoLadron = unRecorrido;
+    	this.ladron = ladron;
+    	this.heridaCuchillo = false;
+    	this.heridaPistola = false;
+    	this.dificultad = nivel_dificultad;
+    	
+    	DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory
+				.newInstance();
+		try {
+			File xmlFile = new File("pistas.xml");
 
             DocumentBuilder documentBuilder = documentBuilderFactory
                     .newDocumentBuilder();
@@ -68,6 +70,17 @@ public class CreadorDePistas {
             NodeList listaPistas = document.getElementsByTagName("Pista");
 
             Element e = (Element) listaPistas.item(0);
+
+				
+    	if( dificultad == Valor.COMUN)
+    		this.crearPistasFaciles();
+    	
+    	else if( dificultad == Valor.VALIOSO)
+    		this.crearPistasIntermedias();
+    	
+    	else this.crearPistasDificiles();
+    	
+    	
 
             if (ladron.obtenerDescripcion().getSenia() == Senia.JOYAS)
                 textoSenia = e.getAttribute("textoSeniaJoyas");
@@ -122,56 +135,59 @@ public class CreadorDePistas {
         Collections.shuffle(this.pistasLadron);
 
     }
+	private void crearPistasFaciles() {
+    	
+		this.aux_heridaCuchillo = 4;
+		this.aux_heridaPistola = 10;
+		
+	}
 
-    private void crearPistasFaciles() {
 
-        this.aux_heridaCuchillo = 3;
-        this.aux_heridaPistola = 10;
-        this.dificultad = Dificultad.FACIL;
 
+	private void crearPistasIntermedias() {
+		
+		this.aux_heridaCuchillo = 4;
+		this.aux_heridaPistola = 5;
+	
+		
+	}
+
+
+	private void crearPistasDificiles() {
+		this.aux_heridaCuchillo = 5;
+		this.aux_heridaPistola = 5;
+		
+	}
+
+    private Pista crearNuevaPista(TipoEdificio tipo){
+    	
+    	if( (this.heridaPistola == true) && Math.random() < 0.3 ){
+    		return crearHeridaPistola();
+    	}
+    	if( (this.heridaCuchillo == true) && Math.random()< 0.3 ) {
+    		return crearHeridaCuchillo();
+    	}
+
+    	if(this.ciudadSiguiente!=null){
+    		if(tipo==TipoEdificio.AEROPUERTO)return crearPistaAeropuerto();
+    		if(tipo==TipoEdificio.PUERTO)return crearPistaPuerto();
+    		if(tipo==TipoEdificio.BANCO)return crearPistaBanco();
+    		if(tipo==TipoEdificio.BOLSA)return crearPistaBolsa();
+    		return crearPistaBiblioteca();
+    	}
+    	return new Pista("Esta cerca");
     }
 
-    private void crearPistasIntermedias() {
-
-        this.aux_heridaCuchillo = 3;
-        this.aux_heridaPistola = 4;
-        this.dificultad = Dificultad.INTERMEDIO;
-
-    }
-
-    private void crearPistasDificiles() {
-
-        this.aux_heridaCuchillo = 3;
-        this.aux_heridaPistola = 5;
-        this.dificultad = Dificultad.DIFICIL;
-
-    }
-
-    private Pista crearNuevaPista(TipoEdificio tipo) {
-
-        if ((this.heridaCuchillo) && Math.random() < 0.3) {
-            return crearHeridaCuchillo();
-        }
-        if ((this.heridaPistola) && Math.random() < 0.4) {
-            return crearHeridaPistola();
-        }
-        if (this.ciudadSiguiente != null) {
-            if (tipo == TipoEdificio.AEROPUERTO) return crearPistaAeropuerto();
-            if (tipo == TipoEdificio.PUERTO) return crearPistaPuerto();
-            if (tipo == TipoEdificio.BANCO) return crearPistaBanco();
-            if (tipo == TipoEdificio.BOLSA) return crearPistaBolsa();
-            return crearPistaBiblioteca();
-        }
-        return new Pista("Estï¿½ cerca");
-    }
 
     private Pista crearHeridaPistola() {
-        return new PistaHeridaPistola();
-    }
+		return new Pista("¡Te han disparado!");
+	}
 
-    private Pista crearHeridaCuchillo() {
-        return new PistaHeridaCuchillo();
-    }
+
+	private Pista crearHeridaCuchillo() {
+		return new Pista("¡Te han arrojado un cuchillo!");
+	}
+
 
     private Pista crearPistaBanco() {
         return this.crearPistaBolsa();
@@ -179,16 +195,16 @@ public class CreadorDePistas {
 
     private Pista crearPistaBolsa() {
 
-        if (this.dificultad == Dificultad.DIFICIL) {
-
-            return new Pista(this.textoIndustria + ciudadSiguiente.obtenerIndustria());
-
+    	if(this.dificultad == Valor.MUY_VALIOSO){
+    		
+        	return new Pista(this.textoIndustria + ciudadSiguiente.obtenerIndustria());
+        	
         }
-
-        if (this.dificultad == Dificultad.INTERMEDIO) {
-            if (Math.random() < 0.5)
-                return new Pista(this.textoIndustria + ciudadSiguiente.obtenerIndustria());
-            return new Pista(this.textoMoneda + ciudadSiguiente.obtenerMoneda());
+        	
+        if(this.dificultad == Valor.VALIOSO){
+        	if(Math.random() < 0.5 )
+        		return new Pista(this.textoIndustria + ciudadSiguiente.obtenerIndustria());
+        	return new Pista(this.textoMoneda + ciudadSiguiente.obtenerMoneda());
         }
         // Caso FACIL
 
@@ -197,90 +213,104 @@ public class CreadorDePistas {
 
     private Pista crearPistaBiblioteca() {
 
-        if (this.dificultad == Dificultad.DIFICIL) {
-
-            return new Pista(this.textoFauna + ciudadSiguiente.obtenerFauna());
-
-        }
-
+    	if(this.dificultad == Valor.MUY_VALIOSO){
+    		
+    		return new Pista(this.textoFauna + ciudadSiguiente.obtenerFauna());
+    	}
         // Caso FACIL o INTERMEDIO
         if (Math.random() < 0.5)
             return new Pista(this.textoLugarinteres + ciudadSiguiente.obtenerPuntosInteres());
         return new Pista(this.textoPersonaje + ciudadSiguiente.obtenerPersonaje());
 
+
     }
 
-    private Pista crearPistaPuerto() {
+   
 
-        if (this.dificultad == Dificultad.DIFICIL) {
 
-            if (Math.random() < 0.8)
-                return new Pista(this.textoIdioma + ciudadSiguiente.obtenerIdioma());
-            return new Pista(this.textoBanderaPuerto + ciudadSiguiente.obtenerBandera());
-        }
+    private Pista crearPistaPuerto(){
+    	
+    	if(this.dificultad == Valor.COMUN){
+    		
+    		if(Math.random() < 0.8 )
+    			return new Pista(this.textoIdioma + ciudadSiguiente.obtenerIdioma());
+    		return new Pista(this.textoBanderaPuerto + ciudadSiguiente.obtenerBandera());
+    	}
+    	
+    	if(this.dificultad == Valor.VALIOSO){
+    		if(Math.random() < 0.5 )
+    			return new Pista(this.textoIdioma + ciudadSiguiente.obtenerIdioma());
+    		return new Pista(this.textoBanderaPuerto + ciudadSiguiente.obtenerBandera());
+    	}
+    	// Caso FACIL
 
-        if (this.dificultad == Dificultad.INTERMEDIO) {
-            if (Math.random() < 0.5)
-                return new Pista(this.textoIdioma + ciudadSiguiente.obtenerIdioma());
-            return new Pista(this.textoBanderaPuerto + ciudadSiguiente.obtenerBandera());
-        }
-        // Caso FACIL
         return new Pista(this.textoBanderaPuerto + ciudadSiguiente.obtenerBandera());
     }
 
-    private Pista crearPistaAeropuerto() {
 
-        if (this.dificultad == Dificultad.DIFICIL) {
-
-            if (Math.random() < 0.8)
-                return new Pista(this.textoIdioma + ciudadSiguiente.obtenerIdioma());
-            return new Pista(this.textoBanderaAeropuerto + ciudadSiguiente.obtenerBandera());
-        }
-
-        if (this.dificultad == Dificultad.INTERMEDIO) {
-            if (Math.random() < 0.5)
-                return new Pista(this.textoIdioma + ciudadSiguiente.obtenerIdioma());
-            return new Pista(this.textoBanderaAeropuerto + ciudadSiguiente.obtenerBandera());
-        }
-        // Caso FACIL
-
+    private Pista crearPistaAeropuerto(){
+    	
+	if(this.dificultad == Valor.MUY_VALIOSO){
+    		
+    		if(Math.random() < 0.8 )
+    			return new Pista(this.textoIdioma + ciudadSiguiente.obtenerIdioma());
+    		return new Pista(this.textoBanderaAeropuerto + ciudadSiguiente.obtenerBandera());
+    	}
+    	
+    	if(this.dificultad == Valor.VALIOSO){
+    		if(Math.random() < 0.5 )
+    			return new Pista(this.textoIdioma + ciudadSiguiente.obtenerIdioma());
+    		return new Pista(this.textoBanderaAeropuerto + ciudadSiguiente.obtenerBandera());
+    	}
+    	// Caso FACIL
+        
         return new Pista(this.textoBanderaAeropuerto + ciudadSiguiente.obtenerBandera());
     }
 
-    private void plantarPistasEnCiudad(Ciudad ciudad, Pista pistaLadron) {
+    private void plantarPistasEnCiudad(Ciudad ciudad, int i) {
+    	//uso i para lograr asegurarme plantar una pista del sospechoso en cada una de las primeras cuatro
+    	//ciudades
+    	Iterator<Pista> iteradorPistaLadron = pistasLadron.iterator();
+    	Pista aux_pistaLadron=null;
 
-        for (Lugar lugar : ciudad.obtenerLugaresDisponibles()) {
+    	
+    	if( i<=4 && i>0 )
+    		aux_pistaLadron = pistasLadron.get(--i); 
+    	 
+        for(Lugar lugar: ciudad.obtenerLugaresDisponibles()){
+        	      	
+        	if( lugar == recorridoLadron.obtenerLugarFinal() ) {
+        		lugar.plantarPista(new Pista("¡Ahí esta el ladron!"));
+        		continue;
+        	}
+        	        		
+        	lugar.plantarPista(crearNuevaPista( lugar.obtenerTipo()).ampliarPista(aux_pistaLadron));
+        	
+        	//Siempre agrego un atributo en una ciudad, y al azar los otros atributos
+        	if(iteradorPistaLadron.hasNext() && Math.random()<0.3 && !heridaCuchillo)
+             	aux_pistaLadron = iteradorPistaLadron.next(); 
+             else aux_pistaLadron=null;
 
-            if (lugar == recorridoLadron.obtenerLugarFinal()) continue;
-
-            lugar.plantarPista(crearNuevaPista(lugar.obtenerTipo()).ampliarPista(pistaLadron));
-
-            pistaLadron = null;
         }
     }
 
 
-    public void plantarPistas() {
 
-        Iterator<Pista> iteradorPistaLadron = pistasLadron.iterator();
-        Pista aux_pistaLadron;
-
-        for (Ciudad ciudad : recorridoLadron.obtenerCiudades()) {
-
-            if (this.aux_heridaCuchillo <= 0) this.heridaCuchillo = true;
-            if (this.aux_heridaPistola <= 0) this.heridaPistola = true;
-
-
-            this.ciudadSiguiente = recorridoLadron.obtenerCiudadSiguiente(ciudad);
-
-            if (iteradorPistaLadron.hasNext())
-                aux_pistaLadron = iteradorPistaLadron.next();
-            else aux_pistaLadron = null;
-
-            this.plantarPistasEnCiudad(ciudad, aux_pistaLadron);
-
-            this.aux_heridaCuchillo--;
-            this.aux_heridaPistola--;
-        }
+public void plantarPistas() {
+	
+	int i=1;
+    for( Ciudad ciudad: recorridoLadron.obtenerCiudades()){
+    	
+    	if( i > this.aux_heridaCuchillo ) this.heridaCuchillo=true;
+    	if( i > this.aux_heridaPistola ) this.heridaPistola=true;
+    	
+    	
+        this.ciudadSiguiente = recorridoLadron.obtenerCiudadSiguiente(ciudad);
+        
+        this.plantarPistasEnCiudad(ciudad,i);
+        
+        i++;
     }
+
+}
 }
