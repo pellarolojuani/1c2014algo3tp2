f30cb5fe12ac89fbb843bf6f342497f3d4cfc84a
chrishaig14 <chrishaig14@5fe92814-5a1e-d52a-0bc9-2235f96b8868>
2014-07-03 17:00:25 +0000

    Creados test de Ciudad.

--- tp2Algo3/test/modelo/geografico/CiudadTest.java
+++ tp2Algo3/test/modelo/geografico/CiudadTest.java
@@ -1,84 +1,112 @@
 package modelo.geografico;
 
-import static org.junit.Assert.*;
-
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
 
 public class CiudadTest {
 
+    private Ciudad unaCiudad;
+    private Ciudad otraCiudad;
+
+    @Before
+    public void setUp() {
+        unaCiudad = new Ciudad("unNombre","unaBandera","unaMoneda","unLugarDeInteres","unPersonaje","unaIndustria","unaFauna","unIdioma",new ArrayList<Lugar>(),10,20);
+        otraCiudad = new Ciudad("","","","","","","","",new ArrayList<Lugar>(),30,40);
+    }
+
     @Test
     public void testObtenerCiudadesDestinoDisponibles() {
-        fail("Not yet implemented");
+        Ciudad otraCiudad=new Ciudad();
+        unaCiudad.agregarCiudadVisitable(otraCiudad);
+        assertEquals(unaCiudad.obtenerCiudadesDestinoDisponibles().get(0),otraCiudad);
     }
 
     @Test
     public void testObtenerLugaresDisponibles() {
-        fail("Not yet implemented");
+        Lugar unLugar = new Lugar(TipoEdificio.BANCO);
+        unaCiudad.agregarLugar(unLugar);
+        assertEquals(unaCiudad.obtenerLugaresDisponibles().get(0), unLugar);
     }
 
     @Test
     public void testDistanciaA() {
-        fail("Not yet implemented");
+        unaCiudad.agregarCiudadVisitable(otraCiudad);
+        otraCiudad.agregarCiudadVisitable(unaCiudad);
+        System.out.println(unaCiudad.distanciaA(otraCiudad));
+        System.out.println(otraCiudad.distanciaA(unaCiudad));
+        //Verificamos que el calculo de la distancia entre las ciudades de el mismo valor, llamando a distanciaA desde ambas, con una toleracia de 1m.
+        assertEquals(unaCiudad.distanciaA(otraCiudad),otraCiudad.distanciaA(unaCiudad),0.001);
     }
 
     @Test
     public void testAgregarCiudadVisitable() {
-        fail("Not yet implemented");
+        unaCiudad.agregarCiudadVisitable(otraCiudad);
+        assertEquals(unaCiudad.obtenerCiudadesDestinoDisponibles().contains(otraCiudad),true);
     }
 
     @Test
     public void testGetNombre() {
-        fail("Not yet implemented");
+        assertEquals(unaCiudad.getNombre(),"unNombre");
     }
 
     @Test
     public void testAgregarLugares() {
-        fail("Not yet implemented");
+        ArrayList<Lugar> lugares=new ArrayList<Lugar>();
+        unaCiudad.agregarLugares(lugares);
+        assertEquals(unaCiudad.obtenerLugaresDisponibles(),lugares);
     }
 
     @Test
     public void testObtenerMoneda() {
-        fail("Not yet implemented");
+        assertEquals(unaCiudad.obtenerIndustria(), "unaIndustria");
     }
 
     @Test
     public void testObtenerIdioma() {
-        fail("Not yet implemented");
+        assertEquals(unaCiudad.obtenerIdioma(), "unIdioma");
     }
 
     @Test
     public void testObtenerIndustria() {
-        fail("Not yet implemented");
+        assertEquals(unaCiudad.obtenerIndustria(),"unaIndustria");
     }
 
     @Test
     public void testObtenerBandera() {
-        fail("Not yet implemented");
+        assertEquals(unaCiudad.obtenerBandera(), "unaBandera");
     }
 
     @Test
     public void testObtenerPuntosInteres() {
-        fail("Not yet implemented");
+        assertEquals(unaCiudad.obtenerPuntosInteres(), "unLugarDeInteres");
     }
 
     @Test
     public void testObtenerPersonaje() {
-        fail("Not yet implemented");
+        assertEquals(unaCiudad.obtenerPersonaje(), "unPersonaje");
     }
 
     @Test
     public void testObtenerFauna() {
-        fail("Not yet implemented");
+        assertEquals(unaCiudad.obtenerFauna(), "unaFauna");
     }
 
     @Test
     public void testObtenerLugar() {
-        fail("Not yet implemented");
+        Lugar unLugar = new Lugar(TipoEdificio.BANCO);
+        unaCiudad.agregarLugar(unLugar);
+        assertEquals(unaCiudad.obtenerLugar(TipoEdificio.BANCO), unLugar);
     }
 
     @Test
     public void testAgregarLugar() {
-        fail("Not yet implemented");
+        Lugar otroLugar=new Lugar(TipoEdificio.AEROPUERTO);
+        unaCiudad.agregarLugar(otroLugar);
+        assertEquals(unaCiudad.obtenerLugaresDisponibles().contains(otroLugar),true);
     }
 
 }
